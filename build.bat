4169bbc370fd5581111b407b27d1b8b9b78ccc32
unorig <euangamble@gmail.com>
2023-03-31 14:26:12 +1100

    Update

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-C:\C64\Assemblers\64tass\64tass.exe -a "C:\C64\Ghostbusters\ghostbusters.prg.1100_64tass.S" -o "C:\C64\Ghostbusters\ghostbusters.prg.1100_64tass.compiled.prg"
-C:\C64\Tools\Packing\exomizer.exe sfx $1100 -x 1 "C:\C64\Ghostbusters\ghostbusters.prg.1100_64tass.compiled.prg" -o "C:\C64\Ghostbusters\ghostbusters.compressed.prg" -p 1
-C:\C64\Emulators\C64Debugger\C64Debugger.exe "C:\C64\Ghostbusters\ghostbusters.compressed.prg"
+..\Assemblers\64tass\64tass.exe -a "ghostbusters.prg.1100.1100_64tass.S" -o "ghostbusters.prg.1100_64tass.compiled.prg"
+..\Packing\exomizer.exe sfx $1100 -x 1 "ghostbusters.prg.1100_64tass.compiled.prg" -o "ghostbusters.compressed.prg" -p 1
+..Emulators\C64Debugger\C64Debugger.exe "ghostbusters.compressed.prg"
+..\Emulators\RetroDebugger.exe -wait 2500 -autojmp -prg "ghostbusters.compressed.prg" 
